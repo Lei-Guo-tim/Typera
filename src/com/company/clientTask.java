3f52a76f0c20279a56f2fac8f421cb37c7008bcd
Lei-Guo-tim <tuniigeneratem@protonmail.com>
2020-12-22 21:22:49 +0100

    clientTask2

--- src/com/company/clientTask.java
+++ src/com/company/clientTask.java
@@ -20,81 +20,78 @@ public class clientTask implements Runnable{
         clientRequest = _clientRequest;}
         @Override
     public void run() {
-            try{
-                // Connect with server
-                Socket connection = new Socket(ip, port);
-                System.out.println("Connected! " + connection);
+                try {
+                    // Connect with server
+                    Socket connection = new Socket(ip, port);
+                    System.out.println("Connected! " + connection);
 
 
-                // Setting up input and output streams
-                DataOutputStream outputChoice = new DataOutputStream(connection.getOutputStream());
-                DataOutputStream outputName = new DataOutputStream(connection.getOutputStream());
-                DataOutputStream outputPass = new DataOutputStream(connection.getOutputStream());
-                DataOutputStream outputNewPass = new DataOutputStream(connection.getOutputStream());
-                DataInputStream inputStr = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
-                DataInputStream registerMess = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
-                DataInputStream loginMess = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
-                //Login
-                if(clientRequest.equals("register")) {
-                    Scanner inputName = new Scanner(System.in);
-                    System.out.println("Please enter your username: ");
-                    String username = inputName.next();
-                    Scanner inputPass = new Scanner(System.in);
-                    System.out.println("Please enter your password: ");
-                    String newPassword = inputPass.next();
+                    // Setting up input and output streams
+                    DataOutputStream outputChoice = new DataOutputStream(connection.getOutputStream());
+                    DataOutputStream outputName = new DataOutputStream(connection.getOutputStream());
+                    DataOutputStream outputPass = new DataOutputStream(connection.getOutputStream());
+                    DataOutputStream outputNewPass = new DataOutputStream(connection.getOutputStream());
+                    DataInputStream inputStr = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                    DataInputStream registerMess = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                    DataInputStream loginMess = new DataInputStream(new BufferedInputStream(connection.getInputStream()));
+                    //Login
+                    if (clientRequest.equals("register")) {
+                        Scanner inputName = new Scanner(System.in);
+                        System.out.println("Please enter your username: ");
+                        String username = inputName.next();
+                        Scanner inputPass = new Scanner(System.in);
+                        System.out.println("Please enter your password: ");
+                        String newPassword = inputPass.next();
 
-                    // Start communication
-                    outputChoice.writeUTF(clientRequest); //Writing register, login, exit choice
-                    outputChoice.flush();
-                    outputName.writeUTF(username); //Writing username
-                    outputName.flush();
-                    //Writing password
-                    outputNewPass.writeUTF(newPassword);
-                    outputNewPass.flush();
-                    //User already exists exist message
-                    String regMess = registerMess.readUTF();
-                    System.out.println(regMess);
+                        // Start communication
+                        outputChoice.writeUTF(clientRequest); //Writing register, login, exit choice
+                        outputChoice.flush();
+                        outputName.writeUTF(username); //Writing username
+                        outputName.flush();
+                        //Writing password
+                        outputNewPass.writeUTF(newPassword);
+                        outputNewPass.flush();
+                        //User already exists exist message
+                        String regMess = registerMess.readUTF();
+                        System.out.println(regMess);
 
 
-                } if (clientRequest.equals("login")) {
-                    Scanner inputLog = new Scanner(System.in);
-                    System.out.println("Please enter your username: ");
-                    String username = inputLog.next();
-                    System.out.println("Please enter your password: ");
-                    String password = inputLog.next();
-                    // Start communication
-                    outputChoice.writeUTF(clientRequest); //Writing register, login, exit choice
-                    outputChoice.flush();
-                    outputName.writeUTF(username); //Writing username
-                    outputName.flush();
-                    //Writing password
-                    outputPass.writeUTF(password);
-                    outputPass.flush();
-                    //User exist or does not exist message
-                    String logMess = loginMess.readUTF();
-                    System.out.println(logMess);
+                    }
+                    if (clientRequest.equals("login")) {
+                        Scanner inputLog = new Scanner(System.in);
+                        System.out.println("Please enter your username: ");
+                        String username = inputLog.next();
+                        System.out.println("Please enter your password: ");
+                        String password = inputLog.next();
+                        // Start communication
+                        outputChoice.writeUTF(clientRequest); //Writing register, login, exit choice
+                        outputChoice.flush();
+                        outputName.writeUTF(username); //Writing username
+                        outputName.flush();
+                        //Writing password
+                        outputPass.writeUTF(password);
+                        outputPass.flush();
+                        //User exist or does not exist message
+                        String logMess = loginMess.readUTF();
+                        System.out.println(logMess);
 
-                }else {
-                    System.out.println("Invalid login. Try again.");
+                    } else {
+                        System.out.println("Invalid login. Try again.");
+                    }
+
+                    // Close streams and socket
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                // Close streams and socket
-                outputChoice.close();
-                outputPass.close();
-                outputNewPass.close();
-                outputName.close();
-                inputStr.close();
-                connection.close();
-            } catch (UnknownHostException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
 
+            }
         }
 
 
 
-    }
+
 
 
 
